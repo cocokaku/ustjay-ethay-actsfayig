3bb2de4b971ca29504b3ab94b295fcc829028698
Colleen Kaku <colleenkaku@gmail.com>
2021-02-25 14:32:44 -0800

    Completed pig latin web application project.

--- main.py
+++ main.py
@@ -8,18 +8,25 @@ app = Flask(__name__)
 
 
 def get_fact():
-
     response = requests.get("http://unkno.com")
-
     soup = BeautifulSoup(response.content, "html.parser")
     facts = soup.find_all("div", id="content")
-
     return facts[0].getText()
+
+
+def get_addr(fact):
+    print(fact)
+    payload = {'input_text': fact}
+    response = requests.post('https://hidden-journey-62459.herokuapp.com/piglatinize/', data=payload, allow_redirects=False)
+    return response.headers['Location']
 
 
 @app.route('/')
 def home():
-    return "FILL ME!"
+    fact = get_fact().strip()
+    addr = get_addr(fact)
+    body = "<p><a href={addr}>{addr}</a></p>".format(addr=addr)
+    return body
 
 
 if __name__ == "__main__":
